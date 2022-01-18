d9c612cbe3273267b92945ef9d222016b95e09a4
SAEBOMPAK <saebompak@gmail.com>
2022-01-18 14:33:20 -0800

    fetch api

--- menu/weather/app.js
+++ menu/weather/app.js
@@ -1,15 +1,32 @@
 window.addEventListener('load', ()=> {
-    let long;
+    const API = 'd2b64a537b17543c420bb4847913d68e'
+    let lon;
     let lat;
+    let temperatureDescription = document.querySelector('.teperature-description')
+    let temperatureDegree = document.querySelector('.temperature-degree')
+    let locationTimezone = document.querySelector('.location-timezone')
 
     if(navigator.geolocation) {
         // 웹에서 장치의 위치를 알아낼 때 사용할 수 있는 Geolocation 객체를 반환합니다
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position)
-            long = position.coords.longitude;
-            loat = position.coords.latitude;
+            console.log('position', position)
+
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`
+
+            fetch(api).then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log('data', data)
+                temperatureDegree.textContent = data.main.temp
+            })
         })
     }
 })
 
-// http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d2b64a537b17543c420bb4847913d68e
+// https://www.youtube.com/watch?v=wPElVpR1rwA&t=1283s    14:17
+// https://home.openweathermap.org/api_keys
+
+// http://api.openweathermap.org/data/2.5/weather?q=dublin&appid=d2b64a537b17543c420bb4847913d68e
